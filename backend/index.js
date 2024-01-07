f5d20559d19eeafb683e37916204abddb55c59c6
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-01-07 19:18:31 +0530

    Added frontend

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const { createTodo } = require('./types');
 const { todo } = require('./db')
 const app = express();
+const cors = require('cors');
+
+
 
 app.use(express.json());
+app.use(cors())
 
 app.post("/todo",async (req,res) => {
     const createPayload = req.body;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
+import { Todo } from './components/Todo'
 
 function App() {
+  const [todos,setTodos] = useState();
+
+  fetch("http://localhost:3000/todos")
+  .then(async (res) => {
+     const json = await res.json();
+     setTodos(json.todos);
+  })
 
   return (
     <div>
       <CreateTodo />
+      <Todo todos={todos} />
     </div>
   )
 }
 
 export default App
+ 
